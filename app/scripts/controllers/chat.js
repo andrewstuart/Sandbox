715c74965821903e2817127a5c3006989e5ead14
Andrew Stuart <astuart@gonpl.com>
2014-01-13 14:43:57 -0700

    Enter sends message.

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -5,4 +5,11 @@ angular.module('sandboxApp')
     $scope.currentMessage = Chat.currentMessage;
     $scope.sendMessage = Chat.sendMessage;
     $scope.messages = Chat.messages;
+
+    $scope.keyPress = function(e) {
+      var shifted = false;
+      if(e.keyCode === 13 && !e.shiftKey) {
+        $scope.sendMessage($scope.currentMessage);
+      }
+    };
   }]);
